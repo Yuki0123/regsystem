142a32d0fecd91dfd8948ce7461d9c01062863fd
Yuki0123 <yuki.kobayashi@gmail.com>
2018-08-03 02:09:24 +0900

    add countbutton

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CountButton from './CountButton';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CountButton />, document.getElementById('countbutton'));
 registerServiceWorker();
