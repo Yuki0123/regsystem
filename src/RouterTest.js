756abfa67f436237390372052249404b60dc4b3e
Yuki0123 <yuki.kobayashi@gmail.com>
2018-08-03 17:45:04 +0900

    add RouterTest

---
+++ src/RouterTest.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
+
+const Header = (props) => <div>Header</div>
+const Home = ({ authenticate, isLoggedIn }) => {
+  return (
+    <div>
+      <div>
+        <p>You are now {isLoggedIn ? 'logged in!' : 'logged out!'}</p>
+        <button onClick={authenticate}>
+          { isLoggedIn ? 'Log out' : 'Log in'}
+        </button>
+      </div>
+      <nav>
+        <ul>
+          <li><Link to='/member'>Member Page</Link></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+const requireAuthentication = (WrappedComponent) => {
+  return class EnhancedComponent extends Component {
+    render() {
+      if (this.props.isLoggedIn === false) {
+        return <Redirect to={'/'} />
+      }
+      return <WrappedComponent { ...this.props } />
+    }
+  }
+}
+const MemberPage = () => <h1>Member Page</h1>
+const ProtectedMemberPage = requireAuthentication(MemberPage);
+
+class RouterTest extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={(props) => <Home isLoggedIn={this.state.isLoggedIn} authenticate={this.handleLogin} />} />
+            <Route path="/member" render={(props) => <ProtectedMemberPage isLoggedIn={this.state.isLoggedIn} {...props} />} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default RouterTest;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import CountButton from './CountButton';
+import RouterTest from './RouterTest';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<CountButton />, document.getElementById('countbutton'));
+ReactDOM.render(<RouterTest />, document.getElementById('routertest'));
+
 registerServiceWorker();
